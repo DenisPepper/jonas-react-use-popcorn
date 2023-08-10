d4e93cd83b47d6e80fdfc24b5eb652b6f92a11f9
DenisPepper <thech@yandex.ru>
2023-08-10 20:22:03 +0300

    Добавит стили для рейтинга

--- src/components/star-rating/star-rating.jsx
+++ src/components/star-rating/star-rating.jsx
@@ -1,3 +1,32 @@
-export const StarRating = () => {
-  return <div>⭐⭐⭐</div>;
+const containerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '16px',
+};
+
+const starContainerStyle = {
+  display: 'flex',
+  gap: '4px',
+};
+
+const textStyle = {
+  lineHeight: '1',
+  margin: '0',
+};
+
+const DEFAULT_STARS_COUNT = 5;
+
+export const StarRating = (props) => {
+  const { maxRating = DEFAULT_STARS_COUNT } = props;
+
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span style={textStyle}>⭐{i + 1}</span>
+        ))}
+      </div>
+      <span>😺</span>
+    </div>
+  );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { App } from './components/app/app';
+/* import './index.css';
+import { App } from './components/app/app'; */
+import { StarRating } from './components/star-rating/star-rating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={10} />
   </React.StrictMode>
 );
