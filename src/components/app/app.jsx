b542f26311cc48bac8eb43f4b0db8bee9ff09ed4
DenisPepper <thech@yandex.ru>
2023-08-10 09:32:28 +0300

    Доработает компонент Main

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -27,6 +27,15 @@ const NavBar = () => {
 };
 
 const Main = () => {
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isOpen1, setIsOpen1] = useState(true);
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
   return (
     <main className='main'>
       <div className='box'>
@@ -115,15 +124,6 @@ const Main = () => {
 };
 
 export const App = () => {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen1, setIsOpen1] = useState(true);
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
-
   return (
     <>
       <NavBar />
